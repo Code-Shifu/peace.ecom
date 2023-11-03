9593f08f7ca124da0cdae5373f0e5e73b2fea623
Code-Shifu <egbodopeace0706@gmail.com>
2023-11-03 14:57:13 +0100

    ecom

---
+++ pcom/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom' 
+import Homes from './Pages/Homes'
+
+function App() {
+  return (
+    <>
+    <Homes/>
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
